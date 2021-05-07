947fc5db303276056360365e35a228bb0d187cb6
Adeikalam <46447749+Adeikalam@users.noreply.github.com>
2021-05-07 11:30:50 +0200

    mise en page de l'appli + ajout image dans la présentation + ajout d'une dataviz

--- demo_streamlit.py
+++ demo_streamlit.py
@@ -10,15 +10,13 @@ import pandas as pd
 import numpy as np
 
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 
 from sklearn.model_selection import train_test_split
-
-
-st.title("Voici mon projet de Data Science")
 
 
 df = pd.read_csv("train.csv", index_col = 'PassengerId')
@@ -30,43 +28,65 @@ df['Age'] = df['Age'].fillna(df['Age'].mode()[0])
 
 df = pd.get_dummies(df)
 
-st.write(df)
+page = st.sidebar.radio("", options = ['Présentation', 'Dataviz', 'Modélisation'])
 
-
-X = df.drop('Survived', axis = 1)
-y = df['Survived']
-
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state = 1)
-
-
-def test_model(model):
-    model.fit(X_train, y_train)
+if page == 'Présentation':
+    st.title("Projet Titanic")
     
-    return model.score(X_test, y_test)
+    titanic= plt.imread("titanic.jpg")
+    
+    st.image(titanic)
 
+    st.markdown("""
+                Dans cette application Streamlit nous allons test plusieurs
+                modèles de machine learning sur le dataset du **Titanic**.
+                """)
 
-logreg = LogisticRegression()
-dtree = DecisionTreeClassifier(random_state = 1)
-knn = KNeighborsClassifier()
+if page == 'Dataviz':
+    fig = plt.figure()
+    sns.countplot(df['Survived'])
 
-options = ['Regression Logistique',
-           'Decision Tree',
-           'KNN']
+    st.pyplot(fig)
 
-choix_modele = st.radio("Choisissez un modèle", options = options)
-
-if choix_modele == options[0]:
-    score = test_model(logreg)
-    st.write(score)
+if page == 'Modélisation':
+    # Data Cleaning
+    st.write(df)
     
     
-if choix_modele == options[1]:
-    score = test_model(dtree)
-    st.write(score)
+    X = df.drop('Survived', axis = 1)
+    y = df['Survived']
     
-if choix_modele == options[2]:
-    score = test_model(knn)
-    st.write(score)
+    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state = 1)
+    
+    @st.cache
+    def test_model(model):
+        model.fit(X_train, y_train)
+        
+        return model.score(X_test, y_test)
+    
+    
+    logreg = LogisticRegression()
+    dtree = DecisionTreeClassifier(random_state = 1)
+    knn = KNeighborsClassifier()
+    
+    options = ['Regression Logistique',
+               'Decision Tree',
+               'KNN']
+    
+    choix_modele = st.radio("Choisissez un modèle", options = options)
+    
+    if choix_modele == options[0]:
+        score = test_model(logreg)
+        st.write(score)
+        
+        
+    if choix_modele == options[1]:
+        score = test_model(dtree)
+        st.write(score)
+        
+    if choix_modele == options[2]:
+        score = test_model(knn)
+        st.write(score)
     
     
 
